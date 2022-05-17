e1d6c119b1e5710aa40439a3a4378f67fccf759a
taufiqwahid <taufiq.wahid58@gmail.com>
2022-05-17 15:24:35 +0800

    update code

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
 import {LogBox, StyleSheet} from 'react-native';
 import FlashMessage from 'react-native-flash-message';
+import {Provider} from 'react-redux';
+import store from './src/config/Redux/store';
 import Router from './src/router';
 
 const App = () => {
   LogBox.ignoreAllLogs();
 
   return (
-    <NavigationContainer>
-      <Router />
-      <FlashMessage position="top" />
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Router />
+        <FlashMessage position="top" />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
